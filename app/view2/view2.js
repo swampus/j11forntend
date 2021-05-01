b27b8c15178f555c30617d3e337b1e1c9355f892
swampus <dmitrijs.gavrilovs.swampus@gmail.com>
2021-05-01 12:03:25 +0300

    book delete

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -13,6 +13,11 @@ angular.module('myApp.view2', ['ngRoute'])
 
         $scope.userArray = {};
 
+        $scope.sortByUserName = function(){
+            console.log("abc");
+            $scope.userArray.sort((a, b) => (a.username > b.username) ? 1 : -1);
+        }
+
         $httpClient.get("http://localhost:9000/api/rest/User.svc/users").then(function (response) {
             console.log(response.data);
             $scope.userArray = response.data;
@@ -24,7 +29,7 @@ angular.module('myApp.view2', ['ngRoute'])
         $scope.deleteUser = function (id, username) {
             if (confirm("Are you sure to delete: " + username + " ?")) {
                 $httpClient.put("http://localhost:9000/api/rest/User.svc/user(" + id + ")").then(function (response) {
-                    $scope.userArray= $scope.userArray.filter(user => user.id !== id);
+                    $scope.userArray = $scope.userArray.filter(user => user.id !== id);
                 }).catch(function (error) {
                     console.log("user deleted");
                     //show error DIV
@@ -34,11 +39,45 @@ angular.module('myApp.view2', ['ngRoute'])
             }
         }
 
+        $scope.deleteBook = function (id, bookName) {
+            if (confirm("Are you sure to delete: " + bookName + " ?")) {
+                $httpClient.delete("http://localhost:9000/api/rest/Book.svc/book(" + id + ")")
+                    .then(function (response) {
+                        $scope.userArray.map(user => {
+                            user.bookDTOSet = user.bookDTOSet.filter(book => book.id !== id);
+                        });
+                }).catch(function (error) {
+                    console.log("book deleted");
+                    //show error DIV
+                    alert(error);
+                    console.log(error);
+                });
+            }
+        }
+
+
+        $scope.editUser = function (id) {
+            document.getElementById("tr_"+id).style.background = "yellow";
+            document.getElementById("span_delete_"+id).className = "fa fa-times";
+            document.getElementById("span_edit_"+id).className = "fa fa-save";
+
+            document.getElementById("username_" + id).contentEditable = true;
+            document.getElementById("personalCode_" + id).contentEditable = true;
+            document.getElementById("address_" + id).contentEditable = true;
+            document.getElementById("favoriteBook_" + id).contentEditable = true;
+/*
+            document.getElementById("delete_" + id).contentEditable = true;
+            document.getElementById("edit_" + id).contentEditable = true;
+*/
+
+            console.log(id);
+        }
+
         $scope.showHide = function (id) {
             console.log(document.getElementById(id).style.display);
             if (document.getElementById(id).style.display === 'block') {
                 document.getElementById(id).style.display = 'none';
-            }else{
+            } else {
                 document.getElementById(id).style.display = 'block';
             }
         }
